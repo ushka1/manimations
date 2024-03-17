e149cea00b67c354f3426b6d631b347a6d167180
ushka1 <david.prajzner01@gmail.com>
2024-03-17 13:15:50 +0100

    refeactor

--- animations/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# manim -pql ./stirling_i.py StirlingI
-
-# manim -pql ./stirling_ii.py StirlingII
---
+++ render.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# manim -pql ./src/stirling_i.py StirlingI
+
+# manim -pql ./src/stirling_ii.py StirlingII
+
+# manim -pql ./src/rook_polynomial_1.py RookPolynomial1
